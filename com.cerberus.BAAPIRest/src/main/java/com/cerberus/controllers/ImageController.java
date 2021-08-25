a489b5ac64cdc36f160faea4059b32cc6051410e
Mati <mati.leo@outlook.com>
2021-08-24 23:42:05 -0300

    Crud para images

--- com.cerberus.BAAPIRest/src/main/java/com/cerberus/controllers/ImageController.java
+++ com.cerberus.BAAPIRest/src/main/java/com/cerberus/controllers/ImageController.java
@@ -1,9 +1,8 @@
 package com.cerberus.controllers;
 
 import com.cerberus.models.Image;
-import com.cerberus.models.Order;
+import com.cerberus.models.Image;
 import com.cerberus.services.IImageService;
-import com.cerberus.services.IOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,8 +18,28 @@ public class ImageController {
     @Autowired
     IImageService imageService;
 
+    @GetMapping
+    public Iterable<Image> GetImages() {
+        return imageService.GetImages();
+    }
+
+    @GetMapping("/{id}")
+    public Image GetImageById(@PathVariable Integer id) {
+        return imageService.GetImageById(id);
+    }
+
     @PostMapping
     public Image CreateImage(@RequestBody Image image) {
         return imageService.CreateImage(image);
+    }
+
+    @PutMapping("/{id}")
+    public Image UpdateImage(@RequestBody Image order, @PathVariable Integer id) {
+        return imageService.UpdateImage(order, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void DeleteImage(@PathVariable Integer id) {
+        imageService.DeleteImage(id);
     }
 }
